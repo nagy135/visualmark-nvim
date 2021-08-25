a1fceeecfa131b1035f6dd6ca5b9c085ccb69b52
Viktor Nagy <viktor.nagy@01people.com>
2021-08-25 16:17:31 +0200

    feat: workable demo, toggling, error handling

--- lua/visualmark-nvim/init.lua
+++ lua/visualmark-nvim/init.lua
@@ -1,21 +1,28 @@
-local marks_ns = nil;
+local api = vim.api
 
-local function enable()
-    marks_ns = vim.api.nvim_buf_set_virtual_text(0, 0, 5,{{ "haha", "WarningMsg"}}, {})
-    print('haha testshadsdsa')
-end
-
-local function disable()
-    vim.api.nvim_buf_clear_namespace(0, marks_ns, 0,15)
-    print('haha disable')
-end
-
-local function toggle()
-    print('haha toggle')
-end
-
-return {
-    enable = enable,
-    disable = disable,
-    toggle = toggle,
+local M = {
+    marks_ns = nil
 }
+
+function M:enable()
+    self.marks_ns = vim.api.nvim_buf_set_virtual_text(0, 0, 5,{{ "haha", "WarningMsg"}}, {})
+end
+
+function M:disable()
+    if self.marks_ns == nil then
+        print('Not enabled ...exiting')
+        return
+    end
+    vim.api.nvim_buf_clear_namespace(0, self.marks_ns, 0,15)
+    self.marks_ns = nil
+end
+
+function M:toggle()
+    if self.marks_ns == nil then
+        self:enable()
+    else
+        self:disable()
+    end
+end
+
+return M
--- plugin/visualmark-nvim.vim
+++ plugin/visualmark-nvim.vim
@@ -1,3 +1,3 @@
-nnoremap <leader>me :lua require('visualmark-nvim').enable()<cr>
-nnoremap <leader>md :lua require('visualmark-nvim').disable()<cr>
-nnoremap <leader>mm :lua require('visualmark-nvim').toggle()<cr>
+nnoremap <leader>me :lua require('visualmark-nvim'):enable()<cr>
+nnoremap <leader>md :lua require('visualmark-nvim'):disable()<cr>
+nnoremap <leader>mm :lua require('visualmark-nvim'):toggle()<cr>
